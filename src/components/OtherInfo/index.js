bcce7278b64cb9c45f51c679bf01e87af07f345e
Gabe Cohen <gcc3.14@gmail.com>
2021-02-16 22:30:54 -0600

    Add michelle's explanations

--- src/components/OtherInfo/index.js
+++ src/components/OtherInfo/index.js
@@ -17,10 +17,13 @@ const OtherInfo = (props) => {
 
     const formatter = (s) => '$' + s;
     return <div>
+	<p>
+	The final step in determining which bundle is right for you is to consider the gross margins of each bundle. This is tied to the price of grain at the time of sale. From the remaining choices, the bundle with the highest gross margin (revenue minus the cost of inputs) will be selected as your bundle recommendation. 
+	</p>
 	<MakeInput formatter={formatter} name="inputsBudget" state={inputsBudget} set={setInputsBudget} units="per hectare">
 	Inputs Budget:
     </MakeInput>
-	<MakeInput formatter={formatter} name="plotSize" state={plotSize} set={setPlotSize} units="per hectare">
+	<MakeInput name="plotSize" state={plotSize} set={setPlotSize} units="hectare">
 	Plot Size:
     </MakeInput>
 	<MakeInput formatter={formatter} name="priceOfGrain" state={priceOfGrain} set={setPriceOfGrain} units="per MT">
