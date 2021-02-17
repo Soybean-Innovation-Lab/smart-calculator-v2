6f1116509da156b4711ffcb3709c063e1097db90
Gabe Cohen <gcc3.14@gmail.com>
2021-02-16 22:27:57 -0600

    Make bundle explanation clearer

--- src/components/BundleRecommendation/index.js
+++ src/components/BundleRecommendation/index.js
@@ -9,7 +9,7 @@ const BundleRecommendation = (props) => {
 	  .filter((data) => data.affordable && data.required && !data.undef )
 	  .sort((data1, data2) => data2.margins - data1.margins);
     if (recBundle.length === 0) {
-	return <h1> No Recommended Bundle </h1>;
+	return <h1> No Recommended Bundle (Either nothing is required, or nothing is affordable)</h1>;
     }
 	return <h1>
 	    Bundle Recommendation: <em> {recBundle[0].bundle} </em>
