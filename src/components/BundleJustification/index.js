8f8714f10af0ad35dd12adcf47f3408031493f52
Gabe Cohen <gcc3.14@gmail.com>
2021-02-17 15:42:15 -0600

    Clean gh-pages

--- src/components/BundleJustification/index.js
+++
@@ -1,6 +0,0 @@
-import { BundleTable } from './table'
-
-const BundleJustification = (props) => {
-    return <BundleTable />;
-};
-export default BundleJustification;
--- src/components/Utils/index.js
+++
@@ -1,4 +0,0 @@
-import { MakeInput } from './make_input';
-import { useBundleData, useAllBundleData } from './calculator';
-
-export { MakeInput, useBundleData, useAllBundleData };
